083bfc1a72c33e9099855182d4dbaa60559b223c
natjade24 <70019806+natjade24@users.noreply.github.com>
2020-08-25 19:11:45 +0100

    ENG-1 Created Models

---
+++ src/NetC.JuniorDeveloperExam.Web/Models/BlogComments.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetC.JuniorDeveloperExam.Web.Models
+{
+    public class BlogComments
+    {
+        public string Name { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ src/NetC.JuniorDeveloperExam.Web/Models/BlogPosts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetC.JuniorDeveloperExam.Web.Models
+{
+    public class BlogPosts
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string HtmlContent { get; set; }
+    }
+}
