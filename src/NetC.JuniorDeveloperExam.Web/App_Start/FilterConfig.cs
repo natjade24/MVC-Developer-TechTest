047af512b659778a6cfaa8ef401d7e5576ff5c05
natjade24 <70019806+natjade24@users.noreply.github.com>
2020-08-25 18:49:59 +0100

    ENG-1 Added More Classes, Html and CSS files

---
+++ src/NetC.JuniorDeveloperExam.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace NetC.JuniorDeveloperExam
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ src/NetC.JuniorDeveloperExam.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NetC.JuniorDeveloperExam.MvcApplication" Language="C#" %>
